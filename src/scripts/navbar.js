8a9555f390fee634cca5237324d0cf02832ff82a
Luis Piedrahita <luis.piedrahita@outlook.com>
2021-05-16 11:41:26 -0300

    new navbar

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,23 +1,11 @@
-function hamburger() {
-  let mobileLinks = document.querySelector("#mobileLinks");
-  if (mobileLinks.style.display === "block") {
-    mobileLinks.style.display = "none";
-  } else {
-    mobileLinks.style.display = "block";
-  }
-}
-
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
-  let mobileLinks = document.querySelector("#mobileLinks");
-  let header = document.querySelector("#header");
-  if (prevScrollpos > currentScrollPos) {
+  let header = document.querySelector("header");
+  if (prevScrollpos > currentScrollPos && currentScrollPos >= 0) {
     header.style.top = "0";
-    mobileLinks.style.top = "7em";
   } else {
-    header.style.top = "-150px";
-    mobileLinks.style.top = "-160px";
+    header.style.top = "-250px";
   }
   prevScrollpos = currentScrollPos;
 };
